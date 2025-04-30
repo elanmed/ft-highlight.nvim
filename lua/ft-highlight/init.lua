800ba8f65f2f3d4ef1929d7d283ec0d2b3b462fa
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-30 09:42:28 -0400

    update vim.schedule with vim.fn.getchar

--- lua/ft-highlight/init.lua
+++ lua/ft-highlight/init.lua
@@ -14,25 +14,14 @@ M.setup = function(opts)
 
   --- @param on_key_opts { key: "f"|"F"|"t"|"T", forward: boolean }
   local function on_key(on_key_opts)
-    -- the `schedule` ensures that the highlight is cleared after operator pending mode is complete
-    -- example:
-    -- - in normal mode, `f` is pressed
-    -- - on_key begins to run
-    -- - the clearing cb is scheduled, but not run
-    -- - the highlight is added
-    -- - on_key waits for `f`'s operator before finishing
-    -- - an operator is pressed
-    -- - on_key finishes running
-    -- - the clearing cb is run
-    vim.schedule(function()
-      if ft_highlight.is_highlighted then
-        ft_highlight:maybe_clear_highlight()
-        ft_highlight:toggle_off()
-      end
-    end)
-
     ft_highlight:highlight { forward = on_key_opts.forward, }
-    return on_key_opts.key
+    local input = vim.fn.nr2char(vim.fn.getchar())
+
+    if ft_highlight.is_highlighted then
+      ft_highlight:maybe_clear_highlight()
+      ft_highlight:toggle_off()
+    end
+    return on_key_opts.key .. input
   end
 
   vim.api.nvim_set_hl(0, "FTHighlightFirst", { link = "Normal", })
